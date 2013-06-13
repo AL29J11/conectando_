fff95c0da51451e551db6607b8eac705f4427d8e
Alejandro Ramos Perez <alexithorp@gmail.com>
2013-06-13 08:03:36 -0500

    hola

---
+++ js/acciones.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		$('.enviar').tap(function(){
+			var formulario=$(this).parents('form');
+			var usuario=document.getElementById('Usuario').value;
+			var password=document.getElementById('Password').value;
+			loginconectar(usuario,password);
+			function loginconectar(U,P)
+			{
+				datos="usuario="+U+"&password="+P;
+				$.ajax({
+					type:"post",
+					url:"192.168.1.162/Sitio6/log_in_m.php",
+					data:datos
+				}).done(function(msg){
+					alert(msg);
+					if(msg==""|| msg==null)
+					{alert("usuario incorrecto");}
+					else
+					{alert("Bienvenido");}
+				
+				});//function msg
+			}//function
+		});//tap
+	});//device ready
+});//ready
